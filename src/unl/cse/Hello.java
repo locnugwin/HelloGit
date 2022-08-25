a2da8429c7dd8b79ae86fd2bb3a1c64a0573a05c
Loc Nguyen <lnguyen72@huskers.unl.edu>
2022-08-25 13:01:55 -0500

    Added my major to the output

--- src/unl/cse/Hello.java
+++ src/unl/cse/Hello.java
@@ -3,7 +3,7 @@ package unl.cse;
  * Loc Nguyen (lnguyen72@huskers.unl.edu)
  * 2022/08/25
  * 
- * A simple "Hello World!" program that prints out my name in Java
+ * A simple "Hello World!" program that prints out my name and major in Java
  * @author nloc4
  *
  */
@@ -12,7 +12,8 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello World, I'm Loc Nguyen!");
+		System.out.println("Hello World, I'm Loc Nguyen!\n");
+		System.out.println("My major is Computer Science!\n");
 	}
 
 }
