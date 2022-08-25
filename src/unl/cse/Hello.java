542945458427d67411249ef39713935eafa4b5a5
Loc Nguyen <lnguyen72@huskers.unl.edu>
2022-08-25 12:57:14 -0500

    Initial Commit

---
+++ src/unl/cse/Hello.java
@@ -0,0 +1,18 @@
+package unl.cse;
+/**
+ * Loc Nguyen (lnguyen72@huskers.unl.edu)
+ * 2022/08/25
+ * 
+ * A simple "Hello World!" program that prints out my name in Java
+ * @author nloc4
+ *
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World, I'm Loc Nguyen!");
+	}
+
+}
